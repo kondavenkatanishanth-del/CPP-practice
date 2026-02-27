1921f2e4849d2adfb4fdbd13ff137818369730e0
KONDA VENKATA NISHANTH <kondavenkatanishanth@gmail.com>
2026-02-27 23:01:37 +0530

    Create Even_or_Odd_number.cpp

---
+++ Even_or_Odd_number.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+ using namespace std;
+int main() {
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+    if(n %2 == 0){
+        cout<<n<<" is an Even number";
+    }else{
+        cout<<n<<" is an Odd number";
+    }
+   return 0;
+}
